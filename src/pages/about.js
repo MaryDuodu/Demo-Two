1d96d8645c0d5183d1c9c0e3eaf9c8bcfd421a89
Mary Duodu <osamduodu57@gmail.com>
2022-03-30 19:04:14 +0000

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            style={{
+                paddingTop: '0px',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+                <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+     
+        </div>
+    )
+}
+export default About;
